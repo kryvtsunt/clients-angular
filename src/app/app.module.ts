702f0f2a218b239e729346f085ee1d8891eb0bbc
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-07-25 08:52:59 -0400

    generated empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ClientInfoComponent } from './client-info/client-info.component';
+import { ClientViewerComponent } from './client-viewer/client-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    ClientInfoComponent,
+    ClientViewerComponent
   ],
   imports: [
     BrowserModule
