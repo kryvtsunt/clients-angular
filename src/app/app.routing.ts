f2483d592243ed23d7f66bbde72f0c2e151f06c1
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-07-25 09:53:04 -0400

    added bootstrap; created simple layout

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,8 @@ import {ClientViewerComponent} from './client-viewer/client-viewer.component';
 
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: ClientViewerComponent},
-  // { path: '**', component: ClientViewerComponent}
+  { path: '', redirectTo: 'client', pathMatch: 'full'},
+  { path: 'client', component: ClientViewerComponent},
+  { path: '**', component: ClientViewerComponent}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
